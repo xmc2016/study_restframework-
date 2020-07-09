fc83b4d172d253b505f617a4fe2218d345dae08e
xueminchao <1649214962@qq.com>
2020-07-09 09:59:32 +0800

    django rest framework （mixed-in）的通用视图

--- learn_api/views.py
+++ learn_api/views.py
@@ -3,40 +3,18 @@ from learn_api.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.views import APIView
-from django.http import Http404
+from learn_api.models import Snippet
+from learn_api.serializers import SnippetSerializer
+from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    """
-    列出所有的snippets 或者创建一个新的snippet
-    """
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self,request,*args,**kwargs):
-        return  self.list(request,*args,**kwargs)
-
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
 
 
 
