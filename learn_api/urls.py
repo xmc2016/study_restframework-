b9f63d0faa409472023d9b6a598870c7602c84cb
xueminchao <1649214962@qq.com>
2020-07-10 14:20:08 +0800

    dsf 支持自定义的字段筛选

--- learn_api/urls.py
+++ learn_api/urls.py
@@ -16,8 +16,10 @@ urlpatterns = [
     re_path(r'^snippets/$',views.SnippetList.as_view(),name='snippet-list'),
     re_path(r'^snippets/(?P<pk>[0-9]+)/$',views.SnippetDetail.as_view(),name='snippet-detail'),
     re_path(r'snippets/(?P<pk>[0-9]+)/highlight/$',views.SnippetHighlight.as_view(),name="snippet-highlight"),
+    re_path(r'^purchases/$',views.PurchaseList.as_view(),name='snippet-list'),
     re_path(r'^users/$',views.UserList.as_view(),name='user-list'),
     re_path(r'^user/(?P<pk>[0-9]+)/$',views.UserDetail.as_view(),name='user-detail'),
+
 
 ]
 
--- learn_api/views.py
+++ learn_api/views.py
@@ -13,7 +13,7 @@ from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import renderers
-
+import django_filters.rest_framework
 
 @api_view(['GET'])
 def api_root(request,format=None):
@@ -42,17 +42,20 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get_queryset(self):
-
-        user = self.request.user
-        return Snippet.objects.filter(owner=user)
-
     # 登录后才能创建，更新和删除代码片段
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 #这个perform_create() 可以让用户在通过POST请求创建一个新的Snippet时，
 # 在保存新的Snippet数据的时候会把request中的user赋值给Snippet的owner
     def perform_create(self, serializer):
         serializer.save(owner =self.request.user)
+
+class PurchaseList(generics.ListAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['language','style']
+
 
 
 
@@ -67,6 +70,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
